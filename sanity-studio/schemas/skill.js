12fc6df22c3e4745a74ede967c828df8a6125008
pegah <pegahsafaie@gmail.com>
2021-02-28 11:41:19 +0100

    some small improvements

---
+++ sanity-studio/schemas/skill.js
@@ -0,0 +1,17 @@
+export default {
+  name: "skill",
+  title: "Skill",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "",
+    },
+    {
+      name: "scale", // 1-5
+      title: "Scale",
+      type: "number",
+    },
+  ]
+}
